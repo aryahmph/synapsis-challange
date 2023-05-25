e113f846c6e951d4029c7db86f591102361218d0
Arya Yunanta <aryayun90@gmail.com>
2023-05-25 11:20:21 +0700

    feat(cart): revised cart_items db schema

---
+++ pkg/sql/schema/1684847204_cart_items.down.sql
@@ -0,0 +1,3 @@
+DROP INDEX IF EXISTS unique_cart_product;
+
+DROP TABLE IF EXISTS cart_items;
--- pkg/sql/schema/1684847204_cart_items.up.sql
+++ pkg/sql/schema/1684847204_cart_items.up.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS cart_items
 (
-    id         INT PRIMARY KEY REFERENCES products (id),
+    id         SERIAL PRIMARY KEY,
+    product_id INT       NOT NULL REFERENCES products (id),
     user_id    INT       NOT NULL REFERENCES users (id),
     quantity   INT       NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE UNIQUE INDEX unique_cart_product ON cart_items (id, user_id);
+CREATE UNIQUE INDEX unique_cart_product ON cart_items (user_id, product_id);
