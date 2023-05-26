78310ab9ebfabee97a20d7ac485b9e98d12df7d7
Arya Yunanta <aryayun90@gmail.com>
2023-05-26 16:24:05 +0700

    feat(order): add amount field in create order response data

--- internal/usecase/order/create_order.go
+++ internal/usecase/order/create_order.go
@@ -6,7 +6,7 @@ import (
 	paymentModel "synapsis-challange/internal/model/payment"
 )
 
-func (u orderUsecase) CreateOrder(ctx context.Context, userId int32) (int32, string, int32, error) {
+func (u orderUsecase) CreateOrder(ctx context.Context, userId int32) (int32, string, float64, error) {
 	items, err := u.cartItemRepository.ListCartItemsByUserID(ctx, userId)
 	if err != nil {
 		return 0, "", 0, err
--- internal/usecase/order/usecase.go
+++ internal/usecase/order/usecase.go
@@ -5,5 +5,5 @@ import (
 )
 
 type Usecase interface {
-	CreateOrder(ctx context.Context, userId int32) (int32, string, int32, error)
+	CreateOrder(ctx context.Context, userId int32) (int32, string, float64, error)
 }
